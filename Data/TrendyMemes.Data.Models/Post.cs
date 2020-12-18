e1514b8b58eedecbd12f6f8aa5e1ddf9e3bab1be
Martin <marto7395@gmail.com>
2020-12-18 21:25:36 +0200

    Added Post entity

---
+++ Data/TrendyMemes.Data.Models/Post.cs
@@ -0,0 +1,21 @@
+namespace TrendyMemes.Data.Models
+{
+    using System.Collections.Generic;
+    using TrendyMemes.Data.Common.Models;
+
+    public class Post : BaseDeletableModel<int>
+    {
+        public Post()
+        {
+            this.Tags = new List<Tag>();
+            this.Votes = new List<Vote>();
+            this.Comments = new List<Comment>();
+        }
+
+        public IEnumerable<Tag> Tags { get; set; }
+
+        public IEnumerable<Vote> Votes { get; set; }
+
+        public IEnumerable<Comment> Comments { get; set; }
+    }
+}
