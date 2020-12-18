df328ffdae51770ffc4ba9f238325160806312eb
Martin <marto7395@gmail.com>
2020-12-18 23:46:42 +0200

    Added Posts to Tag

--- Data/TrendyMemes.Data.Models/Tag.cs
+++ Data/TrendyMemes.Data.Models/Tag.cs
@@ -1,12 +1,20 @@
 ﻿namespace TrendyMemes.Data.Models
 {
+    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
 
     using TrendyMemes.Data.Common.Models;
 
     public class Tag : BaseDeletableModel<int>
     {
+        public Tag()
+        {
+            this.Posts = new List<Post>();
+        }
+
         [Required]
         public string Name { get; set; }
+
+        public IEnumerable<Post> Posts { get; set; }
     }
 }
