202c12ac833632cd9ca2244cb10cd63b1901a19d
Martin <marto7395@gmail.com>
2020-12-18 21:25:29 +0200

    Added Comment entity

---
+++ Data/TrendyMemes.Data.Models/Comment.cs
@@ -0,0 +1,18 @@
+namespace TrendyMemes.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using TrendyMemes.Data.Common.Models;
+
+    public class Comment : BaseDeletableModel<int>
+    {
+        [Required]
+        public int PostId { get; set; }
+
+        public Post Post { get; set; }
+
+        [MinLength(3)]
+        [MaxLength(1000)]
+        public string Content { get; set; }
+    }
+}
