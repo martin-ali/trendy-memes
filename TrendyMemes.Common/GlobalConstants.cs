7328d7bcfcdd0295686c2c58b38eea0a5ddc0252
Martin <marto7395@gmail.com>
2020-12-19 13:37:45 +0200

    Added constants for post categorization

--- TrendyMemes.Common/GlobalConstants.cs
+++ TrendyMemes.Common/GlobalConstants.cs
@@ -5,5 +5,9 @@
         public const string SystemName = "TrendyMemes";
 
         public const string AdministratorRoleName = "Administrator";
+
+        public const double TopPostsPercentageInTrendyCategory = 10;
+
+        public const double TopPostsPercentageInRisingCategory = 50;
     }
 }
