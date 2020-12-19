ea66cfb3a09a12e2209bdef12a14eceb598bcc5c
Martin <marto7395@gmail.com>
2020-12-19 13:36:13 +0200

    Implemented PostsController

--- Web/TrendyMemes.Web/Controllers/PostsController.cs
+++ Web/TrendyMemes.Web/Controllers/PostsController.cs
@@ -2,26 +2,47 @@
 {
     using Microsoft.AspNetCore.Mvc;
 
+    using TrendyMemes.Common;
+    using TrendyMemes.Services.Data;
+    using TrendyMemes.Web.ViewModels.Posts;
+
     public class PostsController : BaseController
     {
+        private readonly IPostsService postsService;
+
+        private const string PostsListView = "All";
+
+        public PostsController(IPostsService postsService)
+        {
+            this.postsService = postsService;
+        }
+
         public IActionResult Details(int id)
         {
-            return this.View();
+            var post = this.postsService.GetById<PostDetailsViewModel>(id);
+
+            return this.View(post);
         }
 
         public IActionResult Trendy()
         {
-            return this.View();
+            var posts = this.postsService.GetTopPercent<PostInListViewModel>(GlobalConstants.TopPostsPercentageInTrendyCategory);
+
+            return this.View(PostsListView, posts);
         }
 
         public IActionResult Rising()
         {
-            return this.View();
+            var posts = this.postsService.GetTopPercent<PostInListViewModel>(GlobalConstants.TopPostsPercentageInRisingCategory);
+
+            return this.View(PostsListView, posts);
         }
 
         public IActionResult New()
         {
-            return this.View();
+            var posts = this.postsService.GetAll<PostInListViewModel>();
+
+            return this.View(PostsListView, posts);
         }
     }
 }
