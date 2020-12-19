219e2906cc22fef21f696c9a29933086f788b7bd
Martin <marto7395@gmail.com>
2020-12-19 13:36:36 +0200

    Implemented PostsListViewModel

--- Web/TrendyMemes.Web.ViewModels/Posts/PostsListViewModel.cs
+++ Web/TrendyMemes.Web.ViewModels/Posts/PostsListViewModel.cs
@@ -1,6 +1,14 @@
 ﻿namespace TrendyMemes.Web.ViewModels.Posts
 {
+    using System.Collections.Generic;
+
     public class PostsListViewModel
     {
+        public PostsListViewModel()
+        {
+            this.Posts = new List<PostInListViewModel>();
+        }
+
+        public IEnumerable<PostInListViewModel> Posts { get; set; }
     }
 }
