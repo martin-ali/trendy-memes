44398c5c628c0c4f3d526c62f238604f83aa90c1
Martin <marto7395@gmail.com>
2020-12-19 13:36:26 +0200

    Implemented PostsListViewModel

--- Web/TrendyMemes.Web.ViewModels/Posts/PostInListViewModel.cs
+++ Web/TrendyMemes.Web.ViewModels/Posts/PostInListViewModel.cs
@@ -2,5 +2,12 @@
 {
     public class PostInListViewModel
     {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public string AuthorUsername { get; set; }
     }
 }
