ab12422579c66ae9d961b4c6f1d4c47d44ccc715
Martin <marto7395@gmail.com>
2020-12-26 12:22:52 +0200

    Added UserController

---
+++ Web/TrendyMemes.Web/Areas/Identity/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿namespace TrendyMemes.Web.Areas.Identity.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    using TrendyMemes.Common;
+    using TrendyMemes.Web.Areas.Identity.Services;
+    using TrendyMemes.Web.Controllers;
+
+    [Area(GlobalConstants.IdentityArea)]
+    public class UsersController : BaseController
+    {
+        private readonly IUsersService usersService;
+
+        public UsersController(IUsersService usersService)
+        {
+            this.usersService = usersService;
+        }
+
+        public IActionResult Details(string id)
+        {
+            var user = this.usersService.GetProfileById(id);
+
+            return this.View(user);
+        }
+    }
+}
