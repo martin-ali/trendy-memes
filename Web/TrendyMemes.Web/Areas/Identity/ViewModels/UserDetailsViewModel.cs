e019764fa69f7da3af155e7eaf365261901ce26b
Martin <marto7395@gmail.com>
2020-12-26 12:23:22 +0200

    Added UserDetailsViewModel

---
+++ Web/TrendyMemes.Web/Areas/Identity/ViewModels/UserDetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TrendyMemes.Web.Areas.Identity.ViewModels
+{
+    using TrendyMemes.Data.Models;
+    using TrendyMemes.Services.Mapping;
+
+    public class UserDetailsViewModel : IMapFrom<ApplicationUser>
+    {
+        public string Username { get; set; }
+    }
+}
