f2875c03dd52c916dfa44482839278484d738bff
Martin <marto7395@gmail.com>
2020-12-26 12:21:08 +0200

    Added CommentsService(ICommentsService)

---
+++ Web/TrendyMemes.Web/Areas/Comments/Services/CommentsService.cs
@@ -0,0 +1,31 @@
+﻿namespace TrendyMemes.Web.Areas.Comments.Services
+{
+    using System.Threading.Tasks;
+
+    using TrendyMemes.Data.Common.Repositories;
+    using TrendyMemes.Data.Models;
+    using TrendyMemes.Web.Areas.Comments.ViewModels;
+
+    public class CommentsService : ICommentsService
+    {
+        private readonly IDeletableEntityRepository<Comment> commentsRepository;
+
+        public CommentsService(IDeletableEntityRepository<Comment> commentsRepository)
+        {
+            this.commentsRepository = commentsRepository;
+        }
+
+        public async Task CreateAsync(CommentCreateInputModel input, string authorId)
+        {
+            var comment = new Comment
+            {
+                AuthorId = authorId,
+                PostId = input.PostId,
+                Content = input.Content,
+            };
+
+            await this.commentsRepository.AddAsync(comment);
+            await this.commentsRepository.SaveChangesAsync();
+        }
+    }
+}
---
+++ Web/TrendyMemes.Web/Areas/Comments/Services/ICommentsService.cs
@@ -0,0 +1,11 @@
+﻿namespace TrendyMemes.Web.Areas.Comments.Services
+{
+    using System.Threading.Tasks;
+
+    using TrendyMemes.Web.Areas.Comments.ViewModels;
+
+    public interface ICommentsService
+    {
+        Task CreateAsync(CommentCreateInputModel input, string authorId);
+    }
+}
