a1c2ddcee95cbcc63e82e2736140e49a74ef1df0
Martin <marto7395@gmail.com>
2020-12-26 12:21:48 +0200

    Added Comment ViewModels

---
+++ Web/TrendyMemes.Web/Areas/Comments/ViewModels/CommentCreateInputModel.cs
@@ -0,0 +1,15 @@
+﻿namespace TrendyMemes.Web.Areas.Comments.ViewModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class CommentCreateInputModel
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(1000)]
+        public string Content { get; set; }
+
+        [Required]
+        public int PostId { get; set; }
+    }
+}
---
+++ Web/TrendyMemes.Web/Areas/Comments/ViewModels/CommentListViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace TrendyMemes.Web.ViewModels.Comments
+{
+    using TrendyMemes.Data.Models;
+    using TrendyMemes.Services.Mapping;
+
+    public class CommentListViewModel : IMapFrom<Comment>
+    {
+        public string AuthorId { get; set; }
+
+        public string AuthorUsername { get; set; }
+
+        public string Content { get; set; }
+    }
+}
