94ccbd1ad891042f888917864fa302de2fc2a496
Martin <marto7395@gmail.com>
2020-12-26 12:14:42 +0200

    Added necessary properties to Image

--- Data/TrendyMemes.Data.Models/Image.cs
+++ Data/TrendyMemes.Data.Models/Image.cs
@@ -1,8 +1,23 @@
 ﻿namespace TrendyMemes.Data.Models
 {
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
     using TrendyMemes.Data.Common.Models;
 
-    public class Image : BaseDeletableModel<int>
+    public class Image : BaseDeletableModel<string>
     {
+        public Image()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        [Required]
+        public string Extension { get; set; }
+
+        [Required]
+        public string UserAddedId { get; set; }
+
+        public ApplicationUser UserAdded { get; set; }
     }
 }
