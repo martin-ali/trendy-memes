d487cd1daae4d92ba5b2a24dcef5fdb1b09c0642
Martin <marto7395@gmail.com>
2020-12-26 12:15:03 +0200

    Added data annotations to Setting

--- Data/TrendyMemes.Data.Models/Setting.cs
+++ Data/TrendyMemes.Data.Models/Setting.cs
@@ -1,11 +1,15 @@
 ﻿namespace TrendyMemes.Data.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     using TrendyMemes.Data.Common.Models;
 
     public class Setting : BaseDeletableModel<int>
     {
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Value { get; set; }
     }
 }
