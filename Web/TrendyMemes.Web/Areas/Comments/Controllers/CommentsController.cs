f0b5cb0d9cfaa4bd2364b211cdbab04b4a2331fa
Martin <marto7395@gmail.com>
2020-12-26 12:20:52 +0200

    Added CommentsController

--- Web/TrendyMemes.Web/Areas/Comments/Controllers/CommentsController.cs
+++ Web/TrendyMemes.Web/Areas/Comments/Controllers/CommentsController.cs
@@ -1,9 +1,58 @@
-namespace TrendyMemes.Web.Areas.Comments.Controllers
+﻿namespace TrendyMemes.Web.Areas.Comments.Controllers
 {
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    using TrendyMemes.Common;
+    using TrendyMemes.Data.Models;
+    using TrendyMemes.Web.Areas.Comments.Services;
+    using TrendyMemes.Web.Areas.Comments.ViewModels;
+    using TrendyMemes.Web.Areas.Posts.Controllers;
     using TrendyMemes.Web.Controllers;
 
+    [Area(GlobalConstants.CommentsArea)]
     public class CommentsController : BaseController
     {
+        private readonly ICommentsService commentsService;
+        private readonly UserManager<ApplicationUser> userManager;
 
+        public CommentsController(ICommentsService commentsService, UserManager<ApplicationUser> userManager)
+        {
+            this.commentsService = commentsService;
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Create(CommentCreateInputModel input)
+        {
+            if (this.ModelState.IsValid)
+            {
+                var author = await this.userManager.GetUserAsync(this.User);
+                await this.commentsService.CreateAsync(input, author.Id);
+            }
+
+            return this.RedirectToAction(nameof(PostsController.Details), ControllerHelpers.GetControllerName(nameof(PostsController)), new
+            {
+                area = GlobalConstants.PostsArea,
+                id = input.PostId,
+            });
+        }
+
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return this.View();
+        }
+
+        [HttpGet]
+        [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+        public IActionResult Delete()
+        {
+            return this.View();
+        }
     }
 }
