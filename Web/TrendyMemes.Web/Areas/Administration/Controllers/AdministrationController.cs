c823ca37fdfcaf917a37ebaddfddaa819bd07b27
Martin <marto7395@gmail.com>
2020-12-29 11:55:16 +0200

    Added functionality for creating and Administrator

--- Web/TrendyMemes.Web/Areas/Administration/Controllers/AdministrationController.cs
+++ Web/TrendyMemes.Web/Areas/Administration/Controllers/AdministrationController.cs
@@ -1,14 +1,33 @@
 ﻿namespace TrendyMemes.Web.Areas.Administration.Controllers
 {
+    using System.Threading.Tasks;
+
     using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
 
     using TrendyMemes.Common;
+    using TrendyMemes.Data.Models;
     using TrendyMemes.Web.Controllers;
 
-    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    // [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
     [Area(GlobalConstants.AdministrationArea)]
     public class AdministrationController : BaseController
     {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public AdministrationController(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> BecomeAdmin()
+        {
+            var user = await this.userManager.GetUserAsync(this.User);
+            await this.userManager.AddToRoleAsync(user, GlobalConstants.AdministratorRoleName);
+
+            return this.RedirectToAction(nameof(HomeController), ControllerHelpers.GetControllerName(nameof(HomeController)));
+        }
     }
 }
--- Web/TrendyMemes.Web/Areas/Administration/Controllers/DashboardController.cs
+++ Web/TrendyMemes.Web/Areas/Administration/Controllers/DashboardController.cs
@@ -1,6 +1,9 @@
 ﻿namespace TrendyMemes.Web.Areas.Administration.Controllers
 {
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
+
+    using TrendyMemes.Data.Models;
     using TrendyMemes.Services.Data;
     using TrendyMemes.Web.ViewModels.Administration.Dashboard;
 
@@ -8,7 +11,8 @@
     {
         private readonly ISettingsService settingsService;
 
-        public DashboardController(ISettingsService settingsService)
+        public DashboardController(ISettingsService settingsService, UserManager<ApplicationUser> userManager)
+            : base(userManager)
         {
             this.settingsService = settingsService;
         }
