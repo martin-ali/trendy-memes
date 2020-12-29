bcb9c18f4bfea6c7ef99965109ca1298b8c36889
Martin <marto7395@gmail.com>
2020-12-29 11:55:38 +0200

    Fleshed out UserDetailsViewModel

--- Web/TrendyMemes.Web/Areas/Identity/ViewModels/UserDetailsViewModel.cs
+++ Web/TrendyMemes.Web/Areas/Identity/ViewModels/UserDetailsViewModel.cs
@@ -1,10 +1,18 @@
 ﻿namespace TrendyMemes.Web.Areas.Identity.ViewModels
 {
+    using System.Collections.Generic;
+
     using TrendyMemes.Data.Models;
     using TrendyMemes.Services.Mapping;
 
     public class UserDetailsViewModel : IMapFrom<ApplicationUser>
     {
         public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public ICollection<Post> Posts { get; set; }
+
+        public ICollection<Comment> Comments { get; set; }
     }
 }
