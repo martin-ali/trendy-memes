02321513669bfad262aa372c5ca01f36d227b761
Martin <marto7395@gmail.com>
2020-12-29 11:54:41 +0200

    Added new Home area

--- Web/TrendyMemes.Web/Areas/Home/Controllers/HomeController.cs
+++ Web/TrendyMemes.Web/Areas/Home/Controllers/HomeController.cs
@@ -3,12 +3,14 @@
     using System.Diagnostics;
 
     using Microsoft.AspNetCore.Mvc;
-
+    using TrendyMemes.Common;
     using TrendyMemes.Web.ViewModels;
 
+    [Area(GlobalConstants.HomeArea)]
     public class HomeController : BaseController
     {
-        [HttpGet]
+        [HttpGet("/")]
+        [HttpGet("Home")]
         public IActionResult Index()
         {
             return this.View();
@@ -20,12 +22,11 @@
             return this.View();
         }
 
-        [HttpGet]
+        [HttpGet(nameof(Error))]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return this.View(
-                new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
+            return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
         }
     }
 }
