123d73a39c841428f1c1b3ebc58a4a144ff56c18
Martin <marto7395@gmail.com>
2020-12-29 13:34:16 +0200

    Renamed setupPostCreation to setupPostPreview

--- Web/TrendyMemes.Web/wwwroot/js/site.js
+++ Web/TrendyMemes.Web/wwwroot/js/site.js
@@ -1,9 +1,9 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
 (function start() {
-    function setupPostCreation() {
+    function setupPostPreview() {
 
         const preview = document.getElementById('image-preview');
         const input = document.getElementById('image-upload');
@@ -38,6 +38,6 @@
         });
     }
 
-    setupPostCreation();
+    setupPostPreview();
     setupVoting();
 }())
