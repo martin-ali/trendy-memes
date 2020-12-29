6d784dc2cc92bc7be908f4c9d85ecb1c53a7be90
Martin <marto7395@gmail.com>
2020-12-29 11:53:54 +0200

    Added JavaScript for voting and creating posts(image preview)

--- Web/TrendyMemes.Web/wwwroot/js/site.js
+++ Web/TrendyMemes.Web/wwwroot/js/site.js
@@ -2,3 +2,42 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+(function start() {
+    function setupPostCreation() {
+
+        const preview = document.getElementById('image-preview');
+        const input = document.getElementById('image-upload');
+
+        if (input && preview) {
+
+            input.addEventListener('onchange', (event) => {
+                preview.src = URL.createObjectURL(event.target.files[0]);
+            });
+        }
+    }
+
+    function setupVoting() {
+        let container = document.getElementById("main");
+
+        container.addEventListener('click', (event) => {
+
+            if (event.target.classList
+                && (event.target.classList.contains('upvote')
+                    || event.target.classList.contains('downvote'))) {
+
+                event.preventDefault();
+
+                const parent = event.target.parentElement;
+                const ratingDisplay = parent.getElementsByClassName("rating")[0];
+
+                $.ajax({
+                    url: event.target.href,
+                    success: (newRating) => ratingDisplay.innerText = newRating,
+                });
+            }
+        });
+    }
+
+    setupPostCreation();
+    setupVoting();
+}())
