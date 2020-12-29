a5a9f28202e696eb09ab6262986008b7399a90f0
Martin <marto7395@gmail.com>
2020-12-29 11:51:20 +0200

    Added [Required] annotations to PostTag

--- Data/TrendyMemes.Data.Models/PostTag.cs
+++ Data/TrendyMemes.Data.Models/PostTag.cs
@@ -1,13 +1,17 @@
 namespace TrendyMemes.Data.Models
 {
-    public class PostTag
-    {
-        public int Id { get; set; }
+    using System.ComponentModel.DataAnnotations;
 
+    using TrendyMemes.Data.Common.Models;
+
+    public class PostTag : BaseDeletableModel<int>
+    {
+        [Required]
         public int PostId { get; set; }
 
         public Post Post { get; set; }
 
+        [Required]
         public int TagId { get; set; }
 
         public Tag Tag { get; set; }
