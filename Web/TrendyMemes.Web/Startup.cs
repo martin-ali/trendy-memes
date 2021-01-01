25ecfb7202e708bd601db0ccc2c49e7a35b3b0b2
Tectonik <marto7395@gmail.com>
2021-01-02 00:28:45 +0200

    Ensured database is wiped on every server restart in dev environment

--- Web/TrendyMemes.Web/Startup.cs
+++ Web/TrendyMemes.Web/Startup.cs
@@ -92,6 +92,11 @@
             using (var serviceScope = app.ApplicationServices.CreateScope())
             {
                 var dbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                if (env.IsDevelopment())
+                {
+                    dbContext.Database.EnsureDeleted();
+                }
+
                 dbContext.Database.Migrate();
                 new ApplicationDbContextSeeder().SeedAsync(dbContext, serviceScope.ServiceProvider).GetAwaiter().GetResult();
             }
